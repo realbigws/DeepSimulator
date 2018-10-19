917c316e4f8121e5a9ba7cb0831b8293e552c130
realbigws <wangsheng@ttic.edu>
2018-10-19 15:40:53 +0300

    install.sh mod

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ source deactivate
 conda remove --name basecall --all -y
 conda create --name basecall python=3.6 -y
 source activate basecall
+#wget -q https://mirror.oxfordnanoportal.com/software/analysis/ont_albacore-2.3.1-cp36-cp36m-manylinux1_x86_64.whl
 pip install albacore_2.3.1/ont_albacore-2.3.1-cp36-cp36m-manylinux1_x86_64.whl
+#rm -f ont_albacore-2.3.1-cp36-cp36m-manylinux1_x86_64.whl
 source deactivate
 
